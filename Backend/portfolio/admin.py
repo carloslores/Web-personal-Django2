049248769c8ca81d7feeffd10fdca2d1fa1ad6a4
Carlos Eduardo Lores Hernández <carloseduardoloreshernandez@MacBook-Pro-de-Carlos-2.local>
2019-10-12 11:12:55 +0200

    arreglado el error de guardar proyectos en la base de datos

--- Backend/portfolio/admin.py
+++ Backend/portfolio/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Proyect
+
 
 # Register your models here.
+class ProyectAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+
+admin.site.register(Proyect, ProyectAdmin)
